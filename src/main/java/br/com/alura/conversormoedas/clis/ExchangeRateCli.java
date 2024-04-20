1c870f50e01703a44a0a57dafcad9d3145a1bc86
Adriel <adriel.fsantos@outlook.com>
2024-04-20 20:35:12 -0300

    feat: Add ExchangeRateCli class

---
+++ src/main/java/br/com/alura/conversormoedas/clis/ExchangeRateCli.java
@@ -0,0 +1,89 @@
+package br.com.alura.conversormoedas.clis;
+
+import br.com.alura.conversormoedas.dtos.ConversionResultDto;
+import br.com.alura.conversormoedas.enums.Currency;
+
+import java.util.Scanner;
+
+public class ExchangeRateCli {
+    private static final Scanner sc = new Scanner(System.in);
+
+    public static void showMenu() {
+        System.out.println("*************************************************");
+        System.out.println("Bem vindo(a) ao conversor de moedas!\n");
+        System.out.println("1) Dólar => Peso Argentino");
+        System.out.println("2) Peso Argentino => Dólar");
+        System.out.println("3) Dólar => Real Brasileiro");
+        System.out.println("4) Real brasileiro => Dólar");
+        System.out.println("5) Dólar => Peso Colombiano");
+        System.out.println("6) Peso Colombiano => Dólar");
+        System.out.println("7) Customizar seleção");
+        System.out.println("8) Sair");
+    }
+
+    public static int getUserMenuInput(int min, int max) {
+        int option = 0;
+        while (option < min || option > max) {
+            try {
+                System.out.println("\nEscolha uma opção:");
+                System.out.print(">> ");
+                option = Integer.parseInt(sc.nextLine());
+
+                if (option < min || option > max) {
+                    System.out.println("\n[!] Opção inválida");
+                }
+            } catch (Exception e) {
+                System.out.println("\n[!] Opção inválida");
+            }
+        }
+        return option;
+    }
+
+    public static int getUserMenuInput() {
+        return getUserMenuInput(1, 8);
+    }
+
+    public static double getUserValueInput() {
+        while (true) {
+            try {
+                System.out.println("\nDigite o valor a ser convertido:");
+                double value = sc.nextDouble();
+                sc.nextLine();
+                return value >= 0 ? value : -1 * value;
+            } catch (Exception e) {
+                System.out.println("\n[!] Valor inválido");
+            }
+        }
+
+    }
+
+    public static Currency getUserBaseCurrencyInput() {
+       return getUserCurrencyInput("\nDigite o código da moeda de origem:");
+    }
+
+    public static Currency getUserTargetCurrencyInput() {
+        return getUserCurrencyInput("\nDigite o código da moeda de destino:");
+    }
+
+    private static Currency getUserCurrencyInput(String message) {
+        while (true) {
+            System.out.println(message);
+            String baseCurrencyCode = sc.nextLine().strip();
+
+            try {
+                return Currency.valueOf(baseCurrencyCode);
+            } catch (IllegalArgumentException e) {
+                System.out.println("\n[!] Essa moeda não é suportada pelo sistema");
+            }
+        }
+    }
+
+    public static void showConversionResult(double initialValue, ConversionResultDto result) {
+        System.out.println("\nConversão concluída *****************************");
+        System.out.println("Taxa de conversão: " + result.conversionRate());
+        System.out.printf("Valor original: %.2f %s\n", initialValue, result.baseCode());
+        System.out.printf("Valor convertido: %.2f %s\n", result.conversionResult(), result.targetCode());
+        System.out.println("*************************************************\n");
+
+    }
+}
